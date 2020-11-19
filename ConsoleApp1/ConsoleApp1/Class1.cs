41887058d6b926f0f99bfeadda496e3048a9773f
ElenaDeveloper33 <71463615+ElenaDeveloper33@users.noreply.github.com>
2020-11-20 05:13:10 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Building
+    {
+        public virtual int GetFloors()
+        {
+            return 0;
+        }
+    }
+
+    public class House : Building
+    {
+        public override int GetFloors()
+        {
+            return 1;
+        }
+    }
+
+    public class SkyScraper : House
+    {
+        public override int GetFloors()
+        {
+            return 30;
+        }
+    }
+
+    public class OrdinaryHouse : House
+    {
+        public new int GetFloors()
+        {
+            return 5;
+        }
+    }
+
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Building house1 = new SkyScraper();
+            Console.WriteLine(house1.GetFloors());
+            //30
+
+            House house2 = new SkyScraper();
+            Console.WriteLine(house2.GetFloors());
+            // 30
+
+            House house3 = new OrdinaryHouse();
+            Console.WriteLine(house3.GetFloors());
+            // 1
+
+            //OrdinaryHouse house4 = new House();
+            //Console.WriteLine(house4.GetFloors());
+
+            //int a = 5;
+            //object o = a;
+            //short s = (short)o;
+            //Console.WriteLine(s);
+
+        }
+    }
+}
+
